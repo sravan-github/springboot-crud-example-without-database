673c1222e220f053e4bbcf6dc6adae32a089aa1c
sravan-github <sravan.psra@gmail.com>
2025-06-23 22:24:02 +0530

    Initial commit

---
+++ src/main/java/com/example/springboot_crud_example_without_database/SpringbootCrudExampleWithoutDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_crud_example_without_database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCrudExampleWithoutDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCrudExampleWithoutDatabaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springboot_crud_example_without_database/SpringbootCrudExampleWithoutDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_crud_example_without_database;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCrudExampleWithoutDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
